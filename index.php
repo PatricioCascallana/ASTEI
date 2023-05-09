4c49e0ec0d870a6e765c975bcc5a9e79cf29c7e3
PatricioCascallana <casca@hotmail.com>
2023-05-08 22:50:45 -0300

    enviando

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/estilo.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+    <title>ASTeI - Servicios Telefonicos e Informaticos</title>
+</head>
+<body>
+    <?php
+    include_once "menu.php"; 
+    ?>
+</body>
+</html>
